26b1b758988c613d1bd11f3106d6bf9e26058e53
kaining <42159793+noobying@users.noreply.github.com>
2021-09-30 15:57:07 +0800

    change the data convert

--- tools/data_converters/hico_det.py
+++ tools/data_converters/hico_det.py
@@ -3,6 +3,7 @@ import os.path as osp
 from pathlib import Path
 
 import mmcv
+from PIL import Image
 
 ANNO_FILE = {
     'train': 'annotations/trainval_hico.json',
@@ -19,7 +20,7 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description="Add images' shape into HICO_DET annotations"
     )
-    parser.add_argument('data_root', default='./data/hico_20160224_det/', help='Dataset root')
+    parser.add_argument('--data_root', default='./data/hico_20160224_det/', help='Dataset root')
     parser.add_argument('--data_type', choices=['train', 'test'], default='train', nargs='+',
                         help='Decide which data type to process')
     args = parser.parse_args()
@@ -31,18 +32,17 @@ def add_shape2annotations(data_root, data_type):
     img_prefix = osp.join(data_root, IMG_PREFIX[data_type])
     data_infos = mmcv.load(anno_file)
 
-    progress_bar = mmcv.ProgressBar(len(data_infos))
+    progress_bar = mmcv.ProgressBar(len(data_infos),)
 
     for i, item in enumerate(data_infos):
         filename = osp.join(img_prefix, Path(item['file_name']).name)
-        img = mmcv.imread(filename)
-        width, height = img.shape[:2]
+        img = Image.open(filename)
+        width, height = img.size[:2]
         item['width'], item['height'] = width, height
 
         progress_bar.update()
 
     # TODO: overwrite the origin anno_file
-    # TODO: 使用懒读取 pillow的方法，可不可以变快一点
     mmcv.dump(data_infos, anno_file)
 
 
@@ -51,6 +51,9 @@ def main():
     assert 'hico' in args.data_root, 'This hint that we only consider the HICODet,' \
                                      'So better rename the dataset folder with key name hico'
     assert args.data_type is not None, 'data_type must be specified!'
+    if not isinstance(args.data_type, (list, tuple)):
+        args.data_type = [args.data_type]
+
     for data_type in args.data_type:
         add_shape2annotations(args.data_root, data_type)
 
